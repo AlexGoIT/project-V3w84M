65281a33b3b7187663748ac9ddfcd5bc238c45c5
AlexGoIT <121641029+AlexGoIT@users.noreply.github.com>
2023-11-08 22:38:33 +0000

    Deploying to gh-pages from @ AlexGoIT/project-V3w84M@0bba3308f44229de177bed3b43280c25560b9695 🚀

--- static/js/476.6494d127.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_V3w84M=self.webpackChunkproject_V3w84M||[]).push([[476],{476:function(n,t,e){e.r(t),e.d(t,{default:function(){return u}});var o,r=e(168),i=e(686).Z.h1(o||(o=(0,r.Z)(["\n  margin-top: 72px;\n  font-family: Roboto;\n  color: #efede8;\n  font-size: 32px;\n  font-style: normal;\n  font-weight: 700;\n  line-height: 1.375;\n"]))),f=e(184),l=function(n){var t=n.title;return(0,f.jsx)(i,{children:t})},u=function(){return(0,f.jsx)(l,{title:"Profile Settings"})}}}]);
-//# sourceMappingURL=476.6494d127.chunk.js.map
