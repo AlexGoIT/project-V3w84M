ce2f829e14dfec99e781febf181a701fb863fa0c
AlexGoIT <alexandrpuryaev@gmail.com>
2023-11-09 13:37:01 +0200

    Added Logo component

---
+++ src/components/Logo/Logo.styled.js
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const LogoWrapper = styled(Link)`
+  padding-block: 24px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  @media screen and (min-width: 768px) {
+    padding-block: 32px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-left: 64px;
+  }
+`;
+
+export const LogoIcon = styled.svg(
+  {
+    width: '36px',
+    height: '13px',
+  },
+  ({ iconColor = '#e6533c' }) => ({
+    fill: iconColor,
+    stroke: iconColor,
+  })
+);
+
+export const LogoText = styled.svg`
+  width: 82px;
+  height: 13px;
+
+  @media screen and (min-width: 768px) {
+    width: 100px;
+    height: 16px;
+  }
+`;
