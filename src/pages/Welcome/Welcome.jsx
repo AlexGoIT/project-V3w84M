9b719fda49c8bafae6f9a07b2dae728d0c18a72b
AlexGoIT <alexandrpuryaev@gmail.com>
2023-11-10 22:09:35 +0200

    Added categoriesSlice

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -5,7 +5,30 @@ import WelcomeNavigation from 'components/WelcomeNavigation';
 
 import { WelcomeWrapper } from './Welcome.styled';
 
+//
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { fetchCategories } from 'redux/categories/categoriesOperation';
+import { selectCategories } from 'redux/categories/categoriesSelectors';
+//
 const Welcome = () => {
+  //
+  const ref = useRef(true);
+  const dispatch = useDispatch();
+  const categories = useSelector(selectCategories);
+
+  console.log('Welcome', categories);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current = false;
+      return;
+    }
+
+    dispatch(fetchCategories());
+  }, [dispatch]);
+  //
+
   return (
     <Container>
       <Logo iconColor="#e6533c" />
---
+++ src/redux/categories/categoriesSelectors.js
@@ -0,0 +1,5 @@
+export const selectCategories = state => state.categories.categories;
+
+export const selectIsLoading = state => state.categories.isLoading;
+
+export const selectError = state => state.categories.error;
