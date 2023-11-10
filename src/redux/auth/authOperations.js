fbe031656345cd84d7cf8ee5bcc92fa2c9d82784
AlexGoIT <121641029+AlexGoIT@users.noreply.github.com>
2023-11-10 10:06:05 +0200

    added authSlice, authOperations, authSelectors

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://powerpulse-backend-7hwo.onrender.com';
+
+const tokenControl = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+
+      tokenControl.set(data.token);
+      Notify.success('Registration is successful');
+      return data;
+    } catch (err) {
+      Notify.failure(err.message);
+
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/users/login', credentials);
+
+      tokenControl.set(data.token);
+      Notify.success('Login is successful');
+      return data;
+    } catch (err) {
+      Notify.failure(`Login error: ${err.message}`);
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/users/logout');
+
+    tokenControl.unset();
+    Notify.success('Logout is successful');
+  } catch (err) {
+    Notify.failure(`Logout error: ${err.message}`);
+    return thunkAPI.rejectWithValue(err.message);
+  }
+});
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { login, logout, register } from './authOperations';
 
 const initialState = {
   user: {
-    name: null,
-    email: null,
+    name: '',
+    email: '',
   },
   token: null,
   isAuthorized: false,
@@ -14,7 +15,55 @@ const initialState = {
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(register.pending, state => {
+        state.isRefreshed = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isRefreshed = false;
+        state.isAuthorized = true;
+        state.error = null;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isRefreshed = false;
+        state.error = action.payload;
+      });
+
+    builder
+      .addCase(login.pending, state => {
+        state.isRefreshed = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isRefreshed = false;
+        state.isAuthorized = true;
+        state.error = null;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isRefreshed = false;
+        state.error = action.payload;
+      });
+
+    builder
+      .addCase(logout.pending, state => {
+        state.isRefreshed = true;
+      })
+      .addCase(logout.fulfilled, state => {
+        state.isAuthorized = false;
+        state.isRefreshed = false;
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.error = null;
+      })
+      .addCase(logout.rejected, state => {
+        state.isRefreshed = false;
+        state.error = null;
+      });
+  },
 });
 
 export const authReducer = authSlice.reducer;
