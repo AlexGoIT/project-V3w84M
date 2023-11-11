5ee868f784406a180f237651482fea0de681b4e0
AlexGoIT <alexandrpuryaev@gmail.com>
2023-11-11 23:13:01 +0200

    fixed apiSlice

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -6,44 +6,7 @@ import WelcomeNavigation from 'components/WelcomeNavigation';
 import { WelcomeWrapper } from './Welcome.styled';
 import WelcomeStatistics from 'components/StatisticsInfo';
 
-// Temporary
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-
-import {
-  // fetchCategories,
-  // fetchExercises,
-  // fetchFilters,
-  fetchProducts,
-} from 'redux/api/apiOperations';
-//
-
 const Welcome = () => {
-  console.log('WelcomePage:', 'render');
-  // Temporary
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    console.log('WelcomePage:', 'useEffect');
-
-    // Categories
-    // dispatch(fetchCategories());
-
-    // params - bodypart, muscles,  equipment | pagination - page, limit (default {page: 1, limit: 20})
-    // dispatch(fetchExercises());
-
-    // Filters
-    // dispatch(fetchFilters()); // Повертає всі фільтри
-    // dispatch(fetchFilters({ filter: 'Body parts' })); // Повертає список фільтрів за полем filter
-    // dispatch(fetchFilters({ name: 'Back' })); // Повертає список фільтрів за полем name
-    // dispatch(fetchFilters({ filter: 'Body parts', name: 'Back' })); // Повертає список фільтрів за полем filter і name
-
-    // Products
-    // params - title, category, recomended | pagination - page, limit (default {page: 1, limit: 20})
-    dispatch(fetchProducts());
-  }, [dispatch]);
-  //
-
   return (
     <Container>
       <Logo iconColor="#e6533c" />
