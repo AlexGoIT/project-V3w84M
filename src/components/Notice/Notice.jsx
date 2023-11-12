332c55a0888c4203bc7adcb537ebbbab0df550eb
Max Kotyk <mxm.kotyk@gmail.com>
2023-11-12 19:15:13 +0100

    Added name field to submitting data fixed Notice

--- src/components/Notice/Notice.jsx
+++ src/components/Notice/Notice.jsx
@@ -1,10 +1,5 @@
 import PropTypes from 'prop-types';
-import {
-  NoticeMessage,
-  NoticeArea,
-  NoticeIcon,
-  WarningIcon,
-} from './Notice.styled';
+import { NoticeMessage, NoticeArea, NoticeIcon } from './Notice.styled';
 import sprite from 'assets/images/sprite.svg';
 
 const Notice = ({ notice }) => {
