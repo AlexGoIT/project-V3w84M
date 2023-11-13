7807ca03e27d8ae845f6b0689e267825904b17af
Alexandr Puryaev <alexandrpuryaev@gmail.com>
2023-11-13 13:38:39 +0200

    Update Logout.jsx

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,11 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { LogoutContainer, LogoutButton } from './Logout.styled';
 
 import sprite from 'assets/images/sprite.svg';
+import { logout } from 'redux/auth/authOperations';
 
 const Logout = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(logout());
+  };
+
   return (
     <LogoutContainer>
-      <LogoutButton>Logout</LogoutButton>
+      <LogoutButton onClick={handleClick}>Logout</LogoutButton>
       <svg width="20" height="20" fill="#efede8">
         <use href={`${sprite}#logout`} />
       </svg>
