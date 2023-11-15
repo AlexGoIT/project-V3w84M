559bbaa40531a8d0a60480be285e5c86d0a70773
AlexGoIT <alexandrpuryaev@gmail.com>
2023-11-15 19:37:30 +0200

    Update Loader.styled.js

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -3,12 +3,12 @@ import styled from '@emotion/styled';
 export const WrapRevolvingDot = styled.div`
   position: fixed;
   z-index: 20;
-   top: 0;
+  top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #040404;
+  background-color: rgb(4, 4, 4, 0.3);
   display: flex;
   align-items: center;
-  justify-content: center; 
+  justify-content: center;
 `;
