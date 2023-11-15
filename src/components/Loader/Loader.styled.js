5dc9af2814a35a5add5da71189723074ec77dafe
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-11-15 18:05:14 +0100

    loader fixed

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -3,6 +3,12 @@ import styled from '@emotion/styled';
 export const WrapRevolvingDot = styled.div`
   position: fixed;
   z-index: 20;
-  top: 50%;
-  left: 50%;
+   top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #040404;
+  display: flex;
+  align-items: center;
+  justify-content: center; 
 `;
