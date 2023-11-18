71d21540ccf9d7298841b8694544378ae0715e1a
AlexGoIT <alexandrpuryaev@gmail.com>
2023-11-18 13:33:17 +0200

    Added delete products or exercises from diary

--- src/pages/Diary/Diary.jsx
+++ src/pages/Diary/Diary.jsx
@@ -7,13 +7,41 @@ import TitlePage from 'components/TitlePage';
 import { DiaryContainer, Left, Right, ForPosition } from './Diary.styled';
 import Notice from 'components/Notice';
 
+// import { useDispatch, useSelector } from 'react-redux';
+// import { selectDiaryUpdated } from 'redux/api/apiSelectors';
+// import { useEffect } from 'react';
+// import {
+//   deleteProduct,
+//   diaryUpdateReset,
+//   fetchDiary,
+// } from 'redux/api/apiOperations';
+
 const Diary = () => {
+  // const dispatch = useDispatch();
+  // const dairyUpdated = useSelector(selectDiaryUpdated);
+
+  // console.log('Diary: dairyUpdated =>', dairyUpdated);
+
+  // useEffect(() => {
+  //   if (dairyUpdated) {
+  //     dispatch(fetchDiary({ date: '17-11-2023' }));
+  //   }
+
+  //   dispatch(diaryUpdateReset());
+  // }, [dispatch, dairyUpdated]);
+
+  // const handleClick = () => {
+  //   console.log("Diary: handleClick => '123'");
+  //   dispatch(deleteProduct('123'));
+  // };
+
   return (
     <Container>
       <ForPosition>
         <TitlePage title="Diary" />
         <DaySwitch />
         <DiaryContainer>
+          {/* <button type="button" onClick={handleClick}>Delete</button> */}
           <Left>
             <DayProducts />
             <DayExercises />
